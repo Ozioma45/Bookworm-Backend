223d8529bf157084c33c25baa0f678dde465404b
couragenwanduka <courageobunike@gmail.com>
2024-05-12 11:25:01 +0100

    moving on

--- Bookworm/index.js
+++ Bookworm/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: '*', // Allow requests from this origin
     credentials: true, // Allow credentials (cookies) to be sent
   }));
 app.use(passport.initialize());
